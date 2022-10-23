c0a87b8526eb41608e0ce84c102b826d7ceccd28
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-10-23 16:41:25 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import markup from './markup.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api';
+const apiKey = '30798635-fb0e0813d5f318e7401c3a1d6';
+
+const input = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+
+const guard = document.querySelector('.guard');
+const options = {
+  root: null,
+  rootMargin: '30px',
+};
+
+let lightbox = new SimpleLightbox('.gallery a');
+let tag = null;
+let maxPage = null;
+let page = 1;
+
+input.addEventListener('submit', onSearch);
+const observer = new IntersectionObserver(onScroll, options);
+
+async function onSearch(e) {
+  e.preventDefault();
+  page = 1;
+  tag = e.currentTarget.children[0].children[0].value.trim();
+  gallery.innerHTML = '';
+
+  await getImg(tag);
+
+  lightbox.refresh();
+
+  observer.observe(guard);
+}
+
+async function getImg(tag) {
+  const images = await axios.get(
+    `${BASE_URL}?key=${apiKey}&q=${tag}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+  );
+
+  if (images.data.hits.length === 0)
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+
+  if (page === 1) {
+    Notify.info(`Hooray! We found ${images.data.totalHits} images.`);
+    maxPage = Math.floor(Number(images.data.totalHits) / 40 + 1);
+  }
+
+  gallery.insertAdjacentHTML('beforeend', markup.cards(images.data.hits));
+  page += 1;
+}
+
+async function onScroll(entries) {
+  if (entries[0].isIntersecting && page > 1) {
+    await getImg(tag);
+
+    if (page >= 2) {
+      lightbox.refresh();
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+  }
+  if (maxPage && page > maxPage) {
+    console.log(page);
+    console.log(maxPage);
+    observer.unobserve(guard);
+    Notify.failure(
+      `We're sorry, but you've reached the end of search results.`
+    );
+  }
+}
---
+++ src/markup.js
@@ -0,0 +1,45 @@
+export default { cards };
+
+function cards(images) {
+  return images.reduce(
+    (acc, img) =>
+      acc +
+      `<a clacc="gallery__item" href="${img.largeImageURL}">
+      <div class="gallery-wrap">
+        <img
+          class="gallery__img"
+          src="${img.webformatURL}"
+          alt="${img.tags}"
+          loadind="lazy"
+        />
+        <div class="info">
+          <p class="info-item">
+            <svg width="20" height="20">
+              <use href="./icons.svg#icon-heart"></use>
+            </svg>
+            <b>${img.likes}</b>
+          </p>
+          <p class="info-item">
+            <svg width="20" height="20">
+              <use href="./icons.svg#icon-eye"></use>
+            </svg>
+            <b>${img.views}</b>
+          </p>
+          <p class="info-item">
+            <svg width="20" height="20">
+              <use href="./icons.svg#icon-bubble"></use>
+            </svg>
+            <b>${img.comments}</b>
+          </p>
+          <p class="info-item">
+            <svg width="20" height="20">
+              <use href="./icons.svg#icon-download3"></use>
+            </svg>
+            <b>${img.downloads}</b>
+          </p>
+        </div>
+      </div>
+    </a>`,
+    ''
+  );
+}
